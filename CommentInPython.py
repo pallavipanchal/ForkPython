a4fed7794a072ce0001c472888a67f0dd6c12b9f
pallavipanchal <panchal.pallavi29@gmail.com>
2020-11-07 17:18:05 +0530

    Comments in Python

---
+++ CommentInPython.py
@@ -0,0 +1,14 @@
+def print_fun():
+    # print("Geeks Classes")
+    print("Geeks For Geeks")
+    # print("Geeks Quiz")
+    # print("Geeks")
+    # print("Geeks For")
+
+
+def main():
+    print_fun()
+
+
+if __name__ == "__main__":
+    main()
